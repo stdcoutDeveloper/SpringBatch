0327b4c14b39a306e187a797e77dd0aa3044d670
AnhHV <hoangvietanh@luvina.net>
2025-07-07 10:44:29 +0700

    code UT for import products batch job

--- ch01/src/test/java/com/manning/sbia/ch01/ImportProductsIntegrationTest.java
+++ ch01/src/test/java/com/manning/sbia/ch01/ImportProductsIntegrationTest.java
@@ -10,61 +10,67 @@ import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.ContextConfiguration;
 
 /**
  * @author acogoluegnes
  *
  */
 @SpringBootTest
-@SpringBatchTest
-@ContextConfiguration(locations = { "/import-products-job-context.xml", "/test-context.xml" })
 public class ImportProductsIntegrationTest {
 
-	@Autowired
-	private JobLauncher jobLauncher;
+    @Autowired
+    private JobLauncher jobLauncher;
 
-	@Autowired
-	private Job job;
+    @Autowired
+    private Job job;
 
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-	@BeforeEach
-	public void setUp() throws Exception {
-		jdbcTemplate.update("delete from product");
-		jdbcTemplate.update("insert into product (id,name,description,price) values(?,?,?,?)", "PR....214",
-				"Nokia 2610 Phone", "", 102.23);
-	}
+    @BeforeEach
+    public void setUp() {
+        jdbcTemplate.update("DELETE FROM product");
 
-	@Test
-	public void importProducts() throws Exception {
-		int initial = jdbcTemplate.queryForObject("select count(1) from product", Integer.class);
+        jdbcTemplate.update(
+                "INSERT INTO product (id, name, description, price) VALUES (?, ?, ?, ?)",
+                "PR....214", "Nokia 2610 Phone", "", 102.23
+        );
+    }
 
-		jobLauncher.run(job, new JobParametersBuilder().addString("inputResource", "classpath:/input/products.zip")
-				.addString("targetDirectory", "./target/importproductsbatch/").addString("targetFile", "products.txt")
-				.addLong("timestamp", System.currentTimeMillis()).toJobParameters());
-		int nbOfNewProducts = 7;
-		assertEquals(initial + nbOfNewProducts,
-				jdbcTemplate.queryForObject("select count(1) from product", Integer.class));
-	}
+    @Test
+    public void importProducts() throws Exception {
+        int initial = jdbcTemplate.queryForObject("SELECT COUNT(1) FROM product", Integer.class);
 
-	@Test
-	public void importProductsWithErrors() throws Exception {
-		int initial = jdbcTemplate.queryForObject("select count(1) from product", Integer.class);
+        jobLauncher.run(job, new JobParametersBuilder()
+                .addString("inputResource", "classpath:/input/products.zip")
+                .addString("targetDirectory", "./target/importproductsbatch/")
+                .addString("targetFile", "products.txt")
+                .addLong("timestamp", System.currentTimeMillis())
+                .toJobParameters()
+        );
 
-		jobLauncher.run(job,
-				new JobParametersBuilder().addString("inputResource", "classpath:/input/products_with_errors.zip")
-						.addString("targetDirectory", "./target/importproductsbatch/")
-						.addString("targetFile", "products.txt").addLong("timestamp", System.currentTimeMillis())
-						.toJobParameters());
-		int nbOfNewProducts = 6;
-		assertEquals(initial + nbOfNewProducts,
-				jdbcTemplate.queryForObject("select count(1) from product", Integer.class));
-	}
+        int nbOfNewProducts = 7;
+        int actual = jdbcTemplate.queryForObject("SELECT COUNT(1) FROM product", Integer.class);
+        assertEquals(initial + nbOfNewProducts, actual, "Should import 7 new products");
+    }
 
+    @Test
+    public void importProductsWithErrors() throws Exception {
+        int initial = jdbcTemplate.queryForObject("SELECT COUNT(1) FROM product", Integer.class);
+
+        jobLauncher.run(job, new JobParametersBuilder()
+                .addString("inputResource", "classpath:/input/products_with_errors.zip")
+                .addString("targetDirectory", "./target/importproductsbatch/")
+                .addString("targetFile", "products.txt")
+                .addLong("timestamp", System.currentTimeMillis())
+                .toJobParameters()
+        );
+
+        int nbOfNewProducts = 6;
+        int actual = jdbcTemplate.queryForObject("SELECT COUNT(1) FROM product", Integer.class);
+        assertEquals(initial + nbOfNewProducts, actual, "Should import 6 new products (1 error skipped)");
+    }
 }
